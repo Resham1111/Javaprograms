04478197c2ab7bc1eb1c613a2e926d1366d4f011
user <user@DESKTOP-SRU5P1P>
2019-10-31 17:07:08 +0530

    commit

---
+++ MyJava/src/com/arrays/Demo7.java
@@ -0,0 +1,20 @@
+package com.arrays;
+
+public class Demo7 {
+
+	public static void main(String[] args) {
+		int [] a= {10,20,30};
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+		for(int i=0;i<a.length;i++)
+		{
+			a[i]=a[i]+ 10;
+			System.out.println(a[i]);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MyJava/src/com/arrays/Demo9.java
@@ -0,0 +1,28 @@
+package com.arrays;
+
+public class Demo9 {
+
+	public static void main(String[] args) {
+		int n=257;
+		int size=5;
+		int [] a= new int[size];
+		for(int i=0;i<a.length;n++)
+		{
+			if(n%2==0)
+			{
+			a[i]=n;
+			i++;
+			}
+			
+			
+		}
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
