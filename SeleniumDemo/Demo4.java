c3d33a6febdd1a4a1abc106cfcd210a9a6b9b7dc
user <user@DESKTOP-SRU5P1P>
2019-11-21 11:34:36 +0530

    commit and push

--- SeleniumDemo/Demo4.java
+++ SeleniumDemo/Demo4.java
@@ -1,15 +1,17 @@
-import java.util.ArrayList;
+//login to gmail
 
 
-import java.util.Set;
+
+
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class Demo2 {
+public class Demo4 {
 
 	public static void main(String[] args)throws InterruptedException {
 		
@@ -18,20 +20,19 @@ public class Demo2 {
 		
 		WebDriver driver=new ChromeDriver();//launching the browser
 		driver.manage().window().maximize();//maximize the window
-		driver.get("https://www.facebook.com/");//....loading the url
+		driver.get("https://accounts.google.com/");//....loading the url
 		//Thread.sleep(2000);
 		//Set<String> win =driver.getWindowHandles();
 		//System.out.println(win.getClass());
-		WebElement email=driver.findElement(By.id("email"));
-		email.sendKeys("jadoo");
-		WebElement pas=driver.findElement(By.id("pass"));
-		pas.sendKeys("yyyyyy");
+		WebElement user=driver.findElement(By.id("identifierId"));
+		user.sendKeys("handwritten110@gmail.com",Keys.ENTER);
+		Thread.sleep(2000);
+		WebElement pas=driver.findElement(By.name("password"));
+		pas.sendKeys("qrwett",Keys.ENTER);
+		//WebElement pas=driver.findElement(By.id("pass"));
+		//pas.sendKeys("yyyyyy");
 		//WebElement log=driver.findElement(By.id("loginbutton"));
 		//log.click();
-		
-		
-		
-		
-
 	}
 }
+
